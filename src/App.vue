70a4a61ef8f75f428755caca5aca1b736f309d74
Zane Gomes <semogenaz@gmail.com>
2024-08-02 20:47:23 +1000

    Completed lab 2

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import ZaneGomes from './components/ZaneGomes.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue';
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <JSON />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.wrapper > * {
+  margin-bottom: 20px;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
+
---
+++ src/components/ZaneGomes.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+<h1 class="green">{{ msg }}</h1>
+    <p>
+      Hi, my name is <b>{{ msg }}</b>. As someone who uses reactjs, I find this
+      very interesting...
+    </p>
+</template>
